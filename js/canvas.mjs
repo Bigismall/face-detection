344e5beea541f4dcba30d12ba65df9ccb68bb5ab
Sławomir Rodak <slawomir.rodak@schibsted.pl>
2020-05-27 15:07:21 +0200

    Done - live working demo with Video stream

--- js/canvas.mjs
+++ js/canvas.mjs
@@ -1,5 +1,5 @@
 function drawFace(canvasContext, detectedFaceCollection) {
-  canvasContext.lineWidth = 10;
+  canvasContext.lineWidth = 5;
   detectedFaceCollection.map((face) => {
     canvasContext.strokeStyle = '#cc0';
     canvasContext.strokeRect(face.boundingBox.x, face.boundingBox.y, face.boundingBox.width, face.boundingBox.height);
@@ -7,7 +7,7 @@ function drawFace(canvasContext, detectedFaceCollection) {
     face.landmarks.map((landmark) => {
       const location = landmark.locations[0];
       canvasContext.beginPath();
-      canvasContext.arc(location.x, location.y, 50, 0, 2 * Math.PI);
+      canvasContext.arc(location.x, location.y, 30, 0, 2 * Math.PI);
       canvasContext.stroke();
     });
 
