fd6a20f39e78bcbe158f36bc4f92feb1d57b0e74
Sławomir Rodak <slawomir.rodak@schibsted.pl>
2020-07-03 08:30:42 +0200

    JS Refactoring landmarks are now scrollable

--- js/canvas.mjs
+++ js/canvas.mjs
@@ -1,16 +1,53 @@
+const EYE_TO_FACE = 0.135;
+const MOUTH_TO_FACE_X = 0.5;
+const MOUTH_TO_FACE_Y = 0.16;
+const TWO_PI = 2 * Math.PI;
+
+const drawLandmark = {
+  eye: drawEye,
+  mouth: drawMonth
+};
+
+function drawEye(canvasCtx, location, face) {
+  const eyeRadius = Math.round(face.boundingBox.width * EYE_TO_FACE);
+
+  canvasCtx.strokeStyle = "#cc0";
+  canvasCtx.beginPath();
+  canvasCtx.arc(location.x, location.y, eyeRadius, 0, TWO_PI);
+  canvasCtx.stroke();
+}
+function drawMonth(canvasCtx, location, face) {
+  const mouthWidth = Math.round(face.boundingBox.width * MOUTH_TO_FACE_X);
+  const mouthHeight = Math.round(face.boundingBox.height * MOUTH_TO_FACE_Y);
+
+  canvasCtx.strokeStyle = "#c00";
+  canvasCtx.strokeRect(
+    location.x - mouthWidth / 2,
+    location.y - mouthHeight / 2,
+    mouthWidth,
+    mouthHeight
+  );
+}
+
+function drawBoundingBox(canvasCtx, face) {
+  canvasCtx.strokeStyle = "#0c0";
+  canvasCtx.strokeRect(
+    face.boundingBox.x,
+    face.boundingBox.y,
+    face.boundingBox.width,
+    face.boundingBox.height
+  );
+}
+
 function drawFace(canvasContext, detectedFaceCollection) {
   canvasContext.lineWidth = 5;
-  detectedFaceCollection.map((face) => {
-    canvasContext.strokeStyle = '#cc0';
-    canvasContext.strokeRect(face.boundingBox.x, face.boundingBox.y, face.boundingBox.width, face.boundingBox.height);
-    canvasContext.strokeStyle = '#0c0';
-    face.landmarks.map((landmark) => {
-      const location = landmark.locations[0];
-      canvasContext.beginPath();
-      canvasContext.arc(location.x, location.y, 30, 0, 2 * Math.PI);
-      canvasContext.stroke();
-    });
 
+  detectedFaceCollection.map(face => {
+    drawBoundingBox(canvasContext, face);
+
+    face.landmarks.map(landmark => {
+      drawLandmark[landmark.type](canvasContext, landmark.locations[0], face);
+    });
   });
 }
 
